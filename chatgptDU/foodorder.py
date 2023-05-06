050ca2bddf2cd6a53e5e70cefe6bd473ab29485e
Sean Wu <xiaoyun.wu@gmail.com>
2023-05-05 21:31:41 -0700

    add the example

--- chatgptDU/foodorder.py
+++ chatgptDU/foodorder.py
@@ -15,7 +15,21 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 
 
 def getSkill(user_input, dialog_states, bot_context):
-    return ""
+    prompt = f"""
+Identify the following items from the review text: 
+- Item purchased by reviewer
+- Company that made the item
+
+The review is delimited with triple backticks. \
+Format your response as a JSON object with \
+"Item" and "Brand" as the keys. 
+If the information isn't present, use "unknown" \
+as the value.
+Make your response as short as possible.
+  
+Review text: '''{user_input}'''
+"""
+    return get_completion(prompt)
 
 
 def getSlot(user_input, skill_details):
