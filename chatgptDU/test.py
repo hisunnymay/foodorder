ec70b9a96979087b094863f7f53b58a5d5c948c0
ChangMei <95261974+hisunnymay@users.noreply.github.com>
2023-05-09 22:19:51 +0800

    Update test.py

--- chatgptDU/test.py
+++ chatgptDU/test.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import foodorder
+import json # used in comparing actual outputs and expected outputs
 
 # User inputs for getting a skill
 input_A1 = """Hey there, can I get a coke please?"""
@@ -18,33 +19,141 @@ for i in range(len(inputs)):
     print("Input:", inputs_skiill[i])
     print("Output:", target_skill(inputs_skill[i]), "\n")
 
-# Context
-context_1 = """skill = FoodOrdering"""
-context_2 = """skill = FoodOrdering"""
-context_3 = """skill = FoodOrdering"""
-context_4 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
-context_5 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
-context_6 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
-context_7 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza", "quantity": "2"}]"""
-context_8 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza", "quantity": "2"}]"""
-contexts = [context_1, context_2, context_3, context_4, context_5, context_6, context_7, context_8]
+# The section presents sets of contexts(e.g. c1), user inputs(e.g. u1) and expected result (e.g. e1).
+# When there is no dish selected, inputs the first valid dish without indicating its quantity.
+c1 = """skill = FoodOrdering"""
+i1 = """Sandwich"""
+e1 = """{
+    "skill": "FoodOrdering", 
+    "dishList": [
+        {
+            "dish": {
+                "value": "Sandwich",
+                "leaf": false
+            }
+            
+        }
+    ]
+}"""
 
-# User inputs for getting a slot
-input_B1 = """Sandwich"""
-input_B2 = """Fenta"""
-input_B3 = """Three chicken wings, please."""
-input_B4 = """Can I change it to Greek Pizza?"""
-input_B5 = """Can I get a different pizza, please?"""
-input_B6 = """One is enough"""
-input_B7 = """Can I also get one more Smoked Chicken Pizza, please?"""
-input_B8 = """Grilled Shrimp"""
-inputs_slot = [input_B1, input_B2, input_B3, input_B4, input_B5, input_B6, input_B7, input_B8]
+# When there is no dish selected, inputs the first invalid dish without indicating its quantity.
+c2 = """skill = FoodOrdering"""
+i2 = """Fenta"""
+e2 = """{
+    "skill": "FoodOrdering", 
+    "dishList": [
+        {
+            "dish": {
+                "value": "Invalid"
+            }
+        }
+    ]
+}"""
 
-# Function test for getting a slot
-target_slot = getSlot(types_slot, skills_slot, examples_slot)
+# When there is no dish selected, inputs the first valid dish with indicating its quantity.
+c3 = """skill = FoodOrdering"""
+i3 = """Three chicken wings, please."""
+e3 = """{
+    "skill": "FoodOrdering", 
+    "dishList": [
+        {
+            "dish": {
+                "value": "ChickenWings",
+                "leaf": true
+            },
+            "quantity": "3"
+        }
+    ]
+}"""
+
+# When one dish has been selected but not assigned a quantity, asks to replace the currently selected dish with a new one, without specifying the quantity.
+c4 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
+i4 = """Can I change it to Greek Pizza?"""
+e4 = """{
+    "skill": "SlotUpdate",
+    "originalSlot": "dishList.dish",
+    "oldValue": {
+        "value": "SmokedChickenPizza",
+        "leaf": true
+    },
+    "newValue": {
+        "value": "GreekPizza",
+        "leaf": true
+    }
+}"""
+
+# When one dish has been selected but not assigned a quantity, asks to replace the currently selected dish without specifying the new one and the quantity.
+c5 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
+i5 = """Can I get a different pizza, please?"""
+e5 = """{
+    "skill": "SlotUpdate",
+    "originalSlot": "dishList.dish",
+    "oldValue": {
+        "value": "SmokedChickenPizza",
+        "leaf": true
+    },
+    "newValue": {
+        "value": "Pizza",
+        "leaf": false
+    }
+}"""
+
+# When one dish has been selected but not assigned a quantity, provides the quantity of the currently selected dish.
+c6 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza"}]"""
+i6 = """One is enough"""
+e6 = """{
+    "skill": "FoodOrdering", 
+    "dishList": [
+        {
+            "dish": "SmokedChickenPizza", 
+            "quantity": "1"
+        }
+    ]
+}"""
+
+# When one dish has been selected with an assigned quantity, asks to change the quantity of the currently selected dish.
+c7 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza", "quantity": "2"}]"""
+i7 = """Can I also get one more Smoked Chicken Pizza, please?"""
+e7 = """{
+    "skill": "SlotUpdate", 
+    "originalSlot": "dishList", 
+    "oldValue": [{"dish": "SmokedChickenPizza", "quantity": "2"}],
+    "newValue": [{"dish": "SmokedChickenPizza", "quantity": "3"}]
+}"""
+
+# When one dish has been selected with an assigned quantity, inputs the second valid dish without indicating its quantity.
+c8 = """skill = FoodOrdering, dishList = [{"dish": "SmokedChickenPizza", "quantity": "2"}]"""
+i8 = """Grilled Shrimp"""
+e8 = """{
+    "skill": "FoodOrdering", 
+    "dishList": [
+        {
+            "dish": {
+                "value": "GrilledShrimp",
+                 "leaf": true
+            }
+        }
+    ]
+}"""
+
+inputs = [i1, i2, i3, i4, i5, i6, i7, i8]
+contexts = [c1, c2, c3, c4, c5, c6, c7, c8]
+expected_results = [e1, e2, e3, e4, e5, e6, e7, e8]
+
+# Function test
+target_slot = getSlot(types, skills, examples)
 
 for i in range(len(inputs)):
-    print("# Test", i + 1)
+    output = target_slot(inputs[i], contexts[i], expected_results[i])
+    print("# Test", i + 1, "----------------------------")
     print("Context:", contexts[i])
-    print("Input:", inputs_slot[i])
-    print("Output:", target_slot(inputs_slot[i], contexts[i]), "\n")
+    print("Input:", inputs[i])
+    print("Output:", output, "\n")
+
+    # Compare the output with expected output
+    expected_output = json.loads(expected_results[i])
+    actual_output = json.loads(output)
+    if actual_output == expected_output:
+        print("Result: Passed. \n")
+    else:
+        print("Result: Failed. \nExpected output is: \n", expected_output, "\n")
