a0bc5c6eb4e15a61a7e74abd17855cbd56742a32
ChangMei <95261974+hisunnymay@users.noreply.github.com>
2023-05-10 22:31:26 +0800

    Update foodorder.py

--- chatgptDU/foodorder.py
+++ chatgptDU/foodorder.py
@@ -46,27 +46,33 @@ class getSlot:
 
     def __call__(self, input, context):
         prompt = f"""
-        Based on the context, your task is to identify value for following slots from the user input,
-        assume the current skill is ```{self.skillDisc}```\
-        ```{self.slotInfo}```
+        Based on the context, your task is to convert user input into structured data. 
+        Context is consist of target skill and picked skill.
         
-        Here are some examples:
-        ```{self.examples}```
+        To get the structured data, you need to follow these steps:
         
-        If the slot has a specific type, you should check \
-        whether its type is an entity or a frame. Entities have no slots, while frames do.
-        - If it's an entity, check whether the user's input matches \
-        the candidate values of that type before filling in the slot. \
-        If there is no match, then set the slot's value to "Invalid".
-        - If it's a frame, fill its slot with the user-input value. 
+        1. In ```{self.skills}```, get the slots of the target skill.
+        
+        2. Extract value from the user input to fill the slots of the target skill. 
+          - If you need to include a calculation, please use the following encoding format: 'slotLabel.plus(n)'.
+
+        3. Check whether the slot has a specific type, if so, you should check whether its type is an entity or a frame. Entities have no slots, while frames do.
+          - If it's an entity, check whether the user's input matches the sub-values of that type before filling in the slot. 
+            - There are two kinds of entity values: node and leaf. Node values have sub-values, while leaf values do not.
+            - If the user-input value is not a sub-value for that particular entity, then the slot's value should be set to 'Invalid'.
+          - If it's a frame, fill its slot with the user-input value. 
     
-        Format your response as a JSON object with the skill and \
-        the user-mentioned slot as the keys.
-        Make sure all the value in the JSON object is not null.
+		4. Format your reply as a JSON object, with the slots mentioned by the user used as the keys. 
+        Here are some examples: ```{self.examples}```. Do NOT output your analysis.
+          
+        5. Once you've composed your response, before output it, please verify the following:
+ 			- Your reply is a JSON object.
+			- All values included within the JSON object are non-null.
+			- The slots that you have filled come from the target skill. 
         
-        Types: ```{self.types}``` 
-        
+        Types: ```{self.types}```
         Context: ```{context}```
         User input: ```{input}```
         """
+    
         return get_completion(prompt)
