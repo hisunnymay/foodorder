d5f4ea42b33cad4f083ad5d37d7896ee5e237eca
ChangMei <95261974+hisunnymay@users.noreply.github.com>
2023-05-12 17:48:35 +0800

    Update foodorder.py

--- chatgptDU/foodorder.py
+++ chatgptDU/foodorder.py
@@ -14,59 +14,51 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-# There is no reason to have skill model to be in json.
 class getSkill:
-    def __init__(self, skills, examples):
+    def __init__(self, skills, examples): 
         self.skills = skills
         self.examples = examples
-
     def __call__(self, input):
         prompt = f"""
-        Given the user input, your task is to determine which of these skills can be implied by it. 
-        ```{self.skills}```
-        
-        For example:
-        ```{self.examples}```
-
-        input: ```{input}```
-        """
-        return get_completion(prompt)
-
-
-# There is no reason to include to skill in the response, as each getSlot is called
-# against one of the skill, so we already know that. We just need to get the slot for that
-# skill.
-# what is the context for?
-class getSlot:
-    def __init__(self, types, skills, slots, examples):
-        self.types = types
-        self.skillDisc = skills
-        self.slotInfo = slots
-        self.examples = examples
-
-    def __call__(self, input, context):
-        prompt = f"""
-        Based on the context, your task is to identify value for following slots from the user input,
-        assume the current skill is ```{self.skillDisc}```\
-        ```{self.slotInfo}```
-        
-        Here are some examples:
-        ```{self.examples}```
-        
-        If the slot has a specific type, you should check \
-        whether its type is an entity or a frame. Entities have no slots, while frames do.
-        - If it's an entity, check whether the user's input matches \
-        the candidate values of that type before filling in the slot. \
-        If there is no match, then set the slot's value to "Invalid".
-        - If it's a frame, fill its slot with the user-input value. 
+        Based on the skills, your task is to determine which of these skills \
+        can appropriately handle the user input. 
     
-        Format your response as a JSON object with the skill and \
-        the user-mentioned slot as the keys.
-        Make sure all the value in the JSON object is not null.
-        
-        Types: ```{self.types}``` 
-        
-        Context: ```{context}```
+        You only need to respond with the label of the skill.
+        Here are ```{self.examples}``` you can learn from. 
+
+        Skills: ```{self.skills}```
         User input: ```{input}```
         """
+        return get_completion(prompt)
+    
+
+class getSlot:
+    def __init__(self, types, examples): 
+        self.types = types
+        self.examples = examples
+    def __call__(self, input, targetSkill, targetSlots):
+        prompt = f"""
+        Your task is to find values for each slot in the target skill from the user input.
+        Target skill is: ```{targetSkill}```.
+        The slots to be filled are: ```{targetSlots}```.
+        
+        If the slot has a specific type, you should check whether its type is an entity or a frame. Entities have no slots, while frames do.
+        - If it's an entity, check whether the user's input matches the sub-values of that type before filling in the slot. 
+            - There are two kinds of entity values: node and leaf. Node values have sub-values, while leaf values do not.
+            - If the user-input value is not a sub-value for that particular entity, then the slot's value should be set to 'Invalid'.
+        - If it's a frame, fill its slot with the user-input value. 
+    
+        Format your response as a JSON object with the label of slots in the target skill as the keys.
+        Here are some examples: ```{self.examples}```
+        
+        Once you've composed your response, before output it, please verify the following:
+        - Your reply shoule be a JSON object. Do NOT include your analysis in the response.
+        - Collect all the labels of slots in the target skill and all the keys in your response. Every key must be one of those labels. Delete non-matching keys.
+        - All values included within the JSON object are non-null.
+
+        
+        Types: ```{self.types}```
+        User input: ```{input}``
+        """
+    
         return get_completion(prompt)
